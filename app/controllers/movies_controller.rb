108abe3cb6900e5e45473b4f7322a082287a4b9d
Kurt Pili <kurtpili3@gmail.com>
2022-02-23 11:31:23 -0600

    Used customized routing and modified Movies show/action for controller & view.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    # @movies = Movie.order("average_vote DESC") # Order by average vote in descending order # This creates n+1 problem
+    @movies = Movie.includes(:production_company).all.order("average_vote DESC") # Order by average vote in descending order
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
